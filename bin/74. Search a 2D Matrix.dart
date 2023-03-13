b9001d2c9721078eec48ab914eab94274ce7ce9a
Asael Ponce Silva <asaek60@gmail.com>
2023-03-13 11:10:38 -0600

    74. Search a 2D Matrix

--- bin/74. Search a 2D Matrix.dart
+++ bin/74. Search a 2D Matrix.dart
@@ -11,16 +11,7 @@ void main() {
 
 bool searchMatrix(List<List<int>> matrix, int target) {
   for (int i = 0; i < matrix.length; i++) {
-    // late int ultimoNUmero;
-    // if (matrix[i].length == 1) {
-    //   ultimoNUmero = matrix[i][matrix[i].length - 1];
-    // } else {
-    //   ultimoNUmero = matrix[i][matrix[i].length - 1];
-    // }
     final ultimoNUmero = matrix[i][matrix[i].length - 1];
-
-    print(ultimoNUmero);
-
     if (target <= ultimoNUmero) {
       for (int ii = 0; ii < matrix[i].length; ii++) {
         if (target == matrix[i][ii]) {
